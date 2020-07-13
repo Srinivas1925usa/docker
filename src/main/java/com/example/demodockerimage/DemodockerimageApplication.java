d693d846e5e0b1a98b20125a4e76c889c84907cc
Srinivas1925usa <32499930+Srinivas1925usa@users.noreply.github.com>
2020-07-13 14:49:17 -0700

    initial commit

---
+++ src/main/java/com/example/demodockerimage/DemodockerimageApplication.java
@@ -0,0 +1,23 @@
+package com.example.demodockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemodockerimageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodockerimageApplication.class, args);
+    }
+
+    @RestController
+    static final class MyController {
+        @GetMapping("/docker")
+        String hello() {
+            return "This is docker image srinivases";
+        }
+
+    }
+}
